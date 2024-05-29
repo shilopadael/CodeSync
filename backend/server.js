3ac167651e3a15e99490f8b395864b6301da966b
Shilo Padael <shilop18@gmail.com>
2024-05-29 10:39:55 +0300

    fsd

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: '*', // Use environment variable or default to localhost
+    origin: 'https://codesync1.onrender.com/', // Use environment variable or default to localhost
     methods: ["GET", "POST"]
   }
 });
